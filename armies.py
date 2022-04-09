3517ac36f4cc50f233db9b1c060d320492474ea4
Dennis O'Donnell <djodonnell96@gmail.com>
2022-04-09 10:03:54 -0400

    Updated Battle logic, doubling up issue continues
    Battle logic looks good, still have the issue of attacking and defending armies being used in a Battle being mixed up

---
+++ armies.py
@@ -0,0 +1,69 @@
+import random
+from operator import attrgetter
+
+class Soldier:
+    active = True #Default status for a soldier"
+    #A soldier makes up an army, and consists of their dice roll and a flag if they are active
+    def __init__(self):
+        roll = 0 #By default, hasn't rolled yet"
+
+    
+
+
+    #When the soldier engages in battle, set it's roll to a random dice roll"
+    def engage(self):
+        self.roll = random.randint(1,6)
+        
+    #When the soldier is defeated, set their status to inactive/false"
+    def defeated(self):
+        self.active = False
+        
+    #Displaying Soldier values to command prompt
+    def printSoldier(self):
+        print(self.roll, " ", self.active)
+
+class Army:
+    thisArmy = []
+    
+    #Constructing the army. Checks if valid size, if valid then creates the army
+    def __init__(self,s = None):
+
+        # Checking if we are to create an empty army or an army with soldier
+
+        if s == None:
+            return
+        else:
+            self.size = s
+            
+        # Check for valid army size, create army if valid
+        
+        if (self.size < 1) or (self.size > 3): #Check for valid army size"
+            print("Please enter an army size between 1 and 3")
+        else :#Create an army(array) of s soldiers
+            for x in range(self.size): # check indexing - I might have thought of this is 1 indexing
+                newSoldier = Soldier()
+                self.thisArmy.append(newSoldier)
+
+    #Engage the army in battle aka setting dice rolls for each soldier
+    def engage(self):
+        for s in self.thisArmy:
+            s.engage()
+
+    #Display roll and status for each soldier in the army
+    def armyStatus(self):
+        for s in self.thisArmy:
+            s.printSoldier()
+
+    # sorting the armies by rolls highest to lowest
+    def sort(self):
+        self.thisArmy.sort(key=attrgetter('roll'),  reverse=True)
+                
+        
+        
+
+
+        
+
+
+            
+        
--- battle.py
+++ battle.py
@@ -1,16 +1,50 @@
-import risk_battle
+import armies
 
 class Battle:
-    attacker = Army(1)
-    defender = Army(1)
-    
-    def __init__(self, attacker, defender):
-        self.attacker = attacker
-        if defender.size > 2:
+    # constructor, 
+    def __init__(self, newAttackingArmy, newDefendingArmy):
+
+        # check if attacker army size is between 1 and 3, if so continue on
+        if newAttackingArmy.size < 1 or newAttackingArmy.size > 3:
+            print("Attacking armies must contain 1-3 soldiers")
+        else:
+            self.attacker = newAttackingArmy
+
+        # check if defending army size is 1 or 2, if so continue on
+        if newDefendingArmy.size < 1 or newDefendingArmy.size > 2:
             print("Defending armies can be a maximum size of 2")
         else:
-            self.defender = defender
+            self.defender = newDefendingArmy
 
+    # As the game would go, roll each solder's die and sort in descending order
     def engage(self):
-        attacker.sort(reverse=True)
-        defender.sort(reverse=True)
+        self.attacker.engage()
+        self.defender.engage()
+        self.attacker.sort()
+        self.defender.sort()
+
+        # compare armies and set flags to defeated if necessary
+        x = max(attacker.size,defender.size)
+
+        # Comparing rolls for each soldier. Defender wins ties.
+        for i in range(x):
+            if defender.thisArmy[i].roll >= attacker.thisArmy[i].roll:
+                attacker.thisArmy[i].defeated()
+            elif defender.thisArmy[i].roll < attacker.thisArmy[i].roll:
+                defender.thisArmy[i].defeated()
+
+    def armiesInfo(self):
+        print("Attacker Status:")
+        self.attacker.armyStatus()
+        print("Defender Status:")
+        self.defender.armyStatus()
+
+
+attacker = armies.Army(3)
+defender = armies.Army(2)
+
+newBattle = Battle(attacker,defender)
+
+newBattle.engage()
+
+newBattle.armiesInfo()
